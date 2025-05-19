3afc5222ae5b3211097bc3906b830b96dcc98564
zerodice0 <zerodice0@gmail.com>
2025-05-20 00:31:31 +0900

    feat: App에 Footer 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import './shared/styles/init.css'
 import './App.css'
 import Header from './shared/widgets/header/header'
+import Footer from './shared/widgets/footer/footer'
 
 function App() {
   return (
-    <div className='app-container'>
+    <div className='app'>
       <Header />
+      {/* <div className='container'>
+        <div className="content">
+        <h1>자주 묻는 질문<em>궁금하신 내용을 빠르게 찾아보세요.</em></h1>
+        <i className="sticky-checker"/>
+        <ul className="tabs">
+          <li className="active">
+            <a><span>서비스 도입</span></a>
+          </li>
+          <li>
+            <a><span>서비스 이용</span></a>
+            </li>
+          </ul>
+        </div>
+      </div> */}
+      <Footer />
     </div>
   )
 }
