4f5d812d0871643df7a855515f2042caed27d9d1
zerodice0 <zerodice0@gmail.com>
2025-05-21 08:48:11 +0900

    fix: fetchCategory() 인자값에 tab 추가.

--- src/pages/faq/api/category_api.ts
+++ src/pages/faq/api/category_api.ts
@@ -1,8 +1,8 @@
 
 import type { Category } from "@/pages/faq/model/category_model";
 
-export const fetchCategory = async (): Promise<Category[]> => {
-  const response = await fetch('/category');
+export const fetchCategory = async (tab: string): Promise<Category[]> => {
+  const response = await fetch(`/category?tab=${tab}`);
   const data = await response.json();
 
   return data.map((category: Category) => category);
