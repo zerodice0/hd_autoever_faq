99a0faead115b1ad543c91f35a3ec3b1472bcec8
zerodice0 <zerodice0@gmail.com>
2025-05-21 08:39:09 +0900

    refactor: faq mocking API 리팩토링
    - CONSULT, USAGE에 따른 중복 로직을 하나로 합침

--- src/pages/faq/api/mockup/faq.ts
+++ src/pages/faq/api/mockup/faq.ts
@@ -1,6 +1,7 @@
 import { http, HttpResponse } from 'msw';
 import { SERVICE_USAGE_FAQ } from '@/pages/faq/api/mockup/service_usage_faq';
 import { SERVICE_CONSULT_FAQ } from '@/pages/faq/api/mockup/service_consult_faq';
+import type { Faq } from '@/pages/faq/model/faq_model';
 
 export const faq = [
   http.get("/faq", ({ request }) => {
@@ -8,6 +9,7 @@ export const faq = [
     const limit = url.searchParams.get('limit');
     const offset = url.searchParams.get('offset');
     const tab = url.searchParams.get('tab');
+    const question = url.searchParams.get('question');
     
     if (!limit || !offset || !tab) {
       return HttpResponse.json({
@@ -20,42 +22,33 @@ export const faq = [
     const offsetNumber = Number(offset);
     const prevOffset = offsetNumber - limitNumber > 0 ? offsetNumber - limitNumber : 0;
     
+    let faq: Faq[] = [];
     if (tab === 'USAGE') {
-      const faq = SERVICE_USAGE_FAQ.slice(offsetNumber, offsetNumber + limitNumber);
-      const nextOffset = (offsetNumber + limitNumber) > SERVICE_USAGE_FAQ.length ? 
-        offsetNumber : offsetNumber + limitNumber;
-
-      return HttpResponse.json(
-        {
-          "pageInfo": {
-            "totalRecord": SERVICE_CONSULT_FAQ.length,
-            "offset": offsetNumber,
-            "limit": limitNumber,
-            "prevOffset": prevOffset,
-            "nextOffset": nextOffset,
-          },
-          "data": faq,
-        }
-      );
+      faq = SERVICE_USAGE_FAQ;
+    } else if (tab === 'CONSULT') {
+      faq = SERVICE_CONSULT_FAQ;
     }
 
-    if (tab === 'CONSULT') {
-      const faq = SERVICE_CONSULT_FAQ.slice(offsetNumber, offsetNumber + limitNumber);
-      const nextOffset = (offsetNumber + limitNumber) > SERVICE_CONSULT_FAQ.length ? 
+    let filteredFaq: Faq[] = faq;
+    if (question) {
+      filteredFaq = faq.filter((faq) => faq.question.includes(question));
+    }
+
+    const responseFaq = filteredFaq.slice(offsetNumber, offsetNumber + limitNumber);
+    const nextOffset = (offsetNumber + limitNumber) > SERVICE_USAGE_FAQ.length ? 
         offsetNumber : offsetNumber + limitNumber;
 
-      return HttpResponse.json(
-        {
-          "pageInfo": {
-            "totalRecord": SERVICE_CONSULT_FAQ.length,
-            "offset": offsetNumber,
-            "limit": limitNumber,
-            "prevOffset": prevOffset,
-            "nextOffset": nextOffset,
-          },
-          "data": faq,
-        }
-      );
-    }
+    return HttpResponse.json(
+      {
+        "pageInfo": {
+          "totalRecord": filteredFaq.length,
+          "offset": offsetNumber,
+          "limit": limitNumber,
+          "prevOffset": prevOffset,
+          "nextOffset": nextOffset,
+        },
+        "data": responseFaq,
+      }
+    );
   }),
 ];
