3a67d08b81fd3e3b165b8d337e14e514d7fecb48
zerodice0 <zerodice0@gmail.com>
2025-05-20 23:21:11 +0900

    chore: 서비스 도입, 서비스 이용 카테고리 및 FAQ 목업 데이터 추가

---
+++ src/pages/faq/api/mockup/service_consult_category.ts
@@ -0,0 +1,14 @@
+export const SERVICE_CONSULT_CATEGORY = [
+  {
+    "categoryID": "PRODUCT",
+    "name": "서비스 상품"
+  },
+  {
+    "categoryID": "COUNSELING",
+    "name": "도입 상담"
+  },
+  {
+    "categoryID": "CONTRACT",
+    "name": "계약"
+  }
+]
---
+++ src/pages/faq/api/mockup/service_usage_category.ts
@@ -0,0 +1,30 @@
+export const SERVICE_USAGE_CATEGORY = [
+  {
+    "categoryID": "SIGN_UP",
+    "name": "가입문의"
+  },
+  {
+      "categoryID": "BUSINESS",
+      "name": "비즈니스(업무용)"
+  },
+  {
+      "categoryID": "ACCIDENT",
+      "name": "사고/보험"
+  },
+  {
+      "categoryID": "RESERVATION",
+      "name": "예약/결제"
+  },
+  {
+      "categoryID": "VEHICLE",
+      "name": "차량문의"
+  },
+  {
+      "categoryID": "REFUEL",
+      "name": "충전"
+  },
+  {
+      "categoryID": "COUPON",
+      "name": "쿠폰/기타"
+  }
+];
