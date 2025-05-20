edf4fe8c1c208c49adea1e07c8476f31963e812d
zerodice0 <zerodice0@gmail.com>
2025-05-21 08:48:51 +0900

    feat: category, faq mocking API 가 동작하도록 worker에 추가

--- src/test/worker.ts
+++ src/test/worker.ts
@@ -1,4 +1,10 @@
 import { setupWorker } from "msw/browser";
 import { terms } from "../shared/dialog_policy/api/mockup/terms";
+import { category } from "../pages/faq/api/mockup/category";
+import { faq } from "../pages/faq/api/mockup/faq";
 
-export const worker = setupWorker(...terms);
+export const worker = setupWorker(
+	...terms,
+	...category,
+	...faq
+);
