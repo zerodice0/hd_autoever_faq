0f2ba3ad9628d7bd7927d9f9941b5e6581fcb1d8
zerodice0 <zerodice0@gmail.com>
2025-05-20 21:44:23 +0900

    feat: 이용약관 데이터를 요청하는 terms_api, terms_hooks를 추가
    - hooks가 api에 대한 wrapper 역할을 하므로, api와 동일한 경로에 배치

---
+++ src/shared/terms/api/terms_api.ts
@@ -0,0 +1,15 @@
+import type { Term } from '@/shared/terms/model/terms_model';
+
+export const fetchAdminPrivacy = async (): Promise<Term[]> => {
+  const response = await fetch('/terms?termsClassID=STARTADMIN_ADMIN_PRIVACY');
+  const data = await response.json();
+
+  return data.terms.map((term: Term) => term);
+};
+
+export const fetchJoinServicesUse = async (): Promise<Term[]> => {
+  const response = await fetch('/terms?termsClassID=STARTADMIN_JOIN_SERVICES_USE');
+  const data = await response.json();
+  
+  return data.terms.map((term: Term) => term);
+};
---
+++ src/shared/terms/api/terms_hooks.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { fetchAdminPrivacy, fetchJoinServicesUse } from '@/shared/terms/api/terms_api';
+import type { Term } from '@/shared/terms/model/terms_model';
+
+export const useAdminPrivacy = (): Term[] => {
+  const [adminPrivacyData, setAdminPrivacyData] = useState<Term[]>([]);
+
+  useEffect(() => {
+    fetchAdminPrivacy().then((data: Term[]) => {
+      setAdminPrivacyData(data);
+    });
+  }, []);
+  
+  return adminPrivacyData;
+}
+
+export const useJoinServicesUse = (): Term[] => {
+  const [joinServicesUseData, setJoinServicesUseData] = useState<Term[]>([]);
+
+  useEffect(() => {
+    fetchJoinServicesUse().then((data: Term[]) => {
+      setJoinServicesUseData(data);
+    });
+  }, []);
+
+  return joinServicesUseData;
+}
