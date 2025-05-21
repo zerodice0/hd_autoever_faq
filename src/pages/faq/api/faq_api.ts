b5a83bdd4b1b5369e4dda433cc57d7ea05d473f1
zerodice0 <zerodice0@gmail.com>
2025-05-21 22:15:00 +0900

    feat: faq api 요청 및 응답 수정

--- src/pages/faq/api/faq_api.ts
+++ src/pages/faq/api/faq_api.ts
@@ -1,8 +1,29 @@
-import type { Faq } from "../model/faq_model";
+import type { Faq, FaqResponse } from "@/pages/faq/model/faq_model";
+import type { PageInformation } from "@/pages/faq/model/page_model";
 
-export const fetchFaq = async (): Promise<Faq[]> => {
-  const response = await fetch('/faq');
-  const data = await response.json();
+export const fetchFaqs = async (
+  limit: number,
+  offset: number,
+  tab: string,
+  faqCategory: string | null,
+  question: string | null,
+): Promise<FaqResponse> => {
+  let queryString = `limit=${limit}&offset=${offset}&tab=${tab}`;
+  if (faqCategory) {
+    queryString += `&faqCategory=${faqCategory}`;
+  }
+  if (question) {
+    queryString += `&question=${question}`;
+  }
 
-  return data.map((faq: Faq) => faq);
+  const response = await fetch(`/faq?${queryString}`);
+  const json = await response.json();
+
+  const pageInformation: PageInformation = json.pageInfo;
+  const faqs: Faq[] = json.data;
+
+  return {
+    pageInformation,
+    data: faqs,
+  };
 }
--- src/pages/faq/model/faq_model.ts
+++ src/pages/faq/model/faq_model.ts
@@ -1,7 +1,14 @@
+import type { PageInformation } from '@/pages/faq/model/page_model';
+
 export interface Faq {
   id: number;
   categoryName: string;
   subCategoryName: string;
   question: string;
   answer: string;
+}
+
+export interface FaqResponse {
+  pageInformation: PageInformation;
+  data: Faq[];
 }
