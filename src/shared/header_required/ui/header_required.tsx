119b619af1efd9a273d0153b8c921e00e70e8c93
zerodice0 <zerodice0@gmail.com>
2025-05-23 23:35:44 +0900

    refactor: 필수 입력 항목 h4 요소를 컴포넌트로 분리.

---
+++ src/shared/header_required/ui/header_required.tsx
@@ -0,0 +1,12 @@
+import '@/shared/header_required/ui/header_required.css';
+
+import type { HeaderRequiredProps } from '@/shared/header_required/model/header_required_props';
+
+export const HeaderRequired = ({ title }: HeaderRequiredProps) => {
+  return (
+    <h4 className="heading-4">
+      {title}
+      <i className="required">필수</i>
+    </h4>
+  );
+}
