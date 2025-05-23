693fa885be631a0706b1eee7272f5f2b2fab4864
zerodice0 <zerodice0@gmail.com>
2025-05-24 00:59:23 +0900

    style: CSS 모듈 설정 및 적용

--- src/shared/header_required/ui/header_required.tsx
+++ src/shared/header_required/ui/header_required.tsx
@@ -1,12 +1,12 @@
-import '@/shared/header_required/ui/header_required.css';
+import styles from '@/shared/header_required/ui/header_required.module.css';
 
 import type { HeaderRequiredProps } from '@/shared/header_required/model/header_required_props';
 
 export const HeaderRequired = ({ title, isRequired }: HeaderRequiredProps) => {
   return (
-    <h4 className="heading-4">
+    <h4 className={styles.heading4}>
       {title}
-      {isRequired && <i className="required">필수</i>}
+      {isRequired && <i className={styles.required}>필수</i>}
     </h4>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,9 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   },
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    }
+  }
 })
