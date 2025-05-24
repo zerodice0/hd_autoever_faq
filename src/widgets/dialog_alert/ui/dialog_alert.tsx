f3fec12f96650bf04baefbe74973c74067e635dd
zerodice0 <zerodice0@gmail.com>
2025-05-24 14:46:36 +0900

    refactor: Alert Dialog에 CSS 모듈 적용

--- src/widgets/dialog_alert/ui/dialog_alert.tsx
+++ src/widgets/dialog_alert/ui/dialog_alert.tsx
@@ -1,5 +1,4 @@
-import '@/widgets/dialog_alert/ui/dialog_alert.css';
-import '@/shared/styles/button_group.css';
+import styles from '@/widgets/dialog_alert/ui/dialog_alert.module.css';
 
 import type { DialogAlertProps } from '@/widgets/dialog_alert/model/dialog_alert_model';
 import { XlargeTertiaryButton } from '@/shared/xlarge_tetiary_button/ui/xlarge_tertiary_button';
@@ -10,10 +9,10 @@ export function DialogAlert({ ref, message }: DialogAlertProps) {
   }
 
   return (
-    <dialog className="dialog-wrapper dialog-error" id="error_faq" ref={ref}>
-      <div className="dialog-body">
-        <p className="message">{message}</p>
-        <div className="button-group">
+    <dialog className={styles.dialogWrapper} id="error_faq" ref={ref}>
+      <div className={styles.dialogBody}>
+        <p className={styles.message}>{message}</p>
+        <div className={styles.buttonGroup}>
           <XlargeTertiaryButton 
             onClick={onClickDialogClose}
             buttonText="확인"
